523016fa54352b706759b714f0a8fb3431be2d91
HouShuai.com <120237979@qq.com>
2019-08-20 10:50:00 +0800

    Add files via upload

---
+++ src/ios/CABasicPlugin.m
@@ -0,0 +1,16 @@
+//
+//  CABasicPlugin.m
+//
+//  Created by houshuai on 19-08-14.
+//
+//
+
+#import "CABasicPlugin.h"
+
+@interface CABasicPlugin ()
+
+@end
+
+@implementation CABasicPlugin
+
+@end
