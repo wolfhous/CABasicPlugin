d8dc06c90f58d2d7cefa3c83fe98a5270f0a85b8
HouShuai.com <120237979@qq.com>
2019-08-20 10:49:28 +0800

    Create CABasicPlugin.h

---
+++ src/ios/CABasicPlugin.h
@@ -0,0 +1,16 @@
+//
+//  CABasicPlugin.h
+//
+//  Created by houshuai on 19-04-14.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDV.h>
+
+
+@interface CABasicPlugin : CDVPlugin
+
+
+
+@end
